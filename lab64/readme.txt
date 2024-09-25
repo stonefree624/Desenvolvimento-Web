b91a846ad5c8b5855fd665cc505119c07e4a3896
Leonardo <broglioleonardo@gmail.com>
2024-09-25 15:07:10 -0300

    lab64- testes iniciais do node.js

---
+++ lab64/readme.txt
@@ -0,0 +1,4 @@
+1. Testar a página inicial:
+curl http://127.0.0.1:3000/
+2. Testar a página Sobre:
+curl http://127.0.0.1:3000/about
---
+++ lab64/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
